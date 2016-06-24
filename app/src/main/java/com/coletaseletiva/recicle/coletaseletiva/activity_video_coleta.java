da432a9564767177336a2b0fc4d2feacc367017c
Hudson Leite <hudson_gomes1@hotmail.com>
2016-06-23 22:22:00 -0300

    Adicionando Activity Video e Layout
    Implementando a codificação do activity video, e ajuste no layout
    video... pensando em um layout unico para vídeo.
    
    necessário passar um parâmetro antes de invocar a
    activity_video_coleta... que refere-se ao urlPath do video.

---
+++ app/src/main/java/com/coletaseletiva/recicle/coletaseletiva/activity_video_coleta.java
@@ -0,0 +1,58 @@
+package com.coletaseletiva.recicle.coletaseletiva;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
+import android.media.MediaPlayer.OnPreparedListener;
+
+public class activity_video_coleta extends AppCompatActivity {
+    MediaController mControls;
+    VideoView video;
+    private int posicaoVideo = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_coleta);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Recuperando Dados Params Enviado
+        String params = this.getIntent().getStringExtra("param");
+        //Recuperando VideoView
+        video = (VideoView) findViewById(R.id.vw_coleta);
+        if(mControls == null){
+            mControls = new MediaController(this);//Instanciando objeto Controller Video
+            mControls.setAnchorView(video);
+        }
+        //Setando Controller Media Controller
+        video.setMediaController(mControls);
+        //Setando Path Video
+        video.setVideoURI(Uri.parse(params));
+        //Quando Vídeo estiver pronto Executar//
+        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                video.seekTo(posicaoVideo);//posição inicial do video
+                video.start();//Inicializa o vídeo
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) { //Salvando estado do vídeo
+        super.onSaveInstanceState(outState);
+        outState.putInt("posicaoVideo", video.getCurrentPosition()); //posição corrente do vídeo
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {//Recuperando o estado do vídeo
+        super.onRestoreInstanceState(savedInstanceState);
+        posicaoVideo = savedInstanceState.getInt("posicaoVideo"); //Setando a posição do vídeo
+        video.seekTo(posicaoVideo); //Iniciando o vídeo na posição correta
+    }
+}
